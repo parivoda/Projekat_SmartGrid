a48e139fc0d34352871aa466766c8a5ab131d790
Nikola <parivoda99@gmail.com>
2022-07-07 19:29:56 +0200

    Add project files.

---
+++ Projekat_SmartGrid/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Projekat_SmartGrid
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
